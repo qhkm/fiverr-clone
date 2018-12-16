e96210a1aa74624245dab9d7551a0cf0d9e4f0c0
Qhkm92 <replace-all>
2018-12-16 13:47:18 +0900

    Add new-gig-routes post method to handled new gig created

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const Gig = require('../models/gig');
+const User = require('../models/user');
+const async = require('async');
 
 router.get('/', (req, res, next) => {
     res.render('main/home');
@@ -8,8 +11,33 @@ router.get('/gigs', (req, res) => {
     res.render('main/gigs');
 });
 
-router.get('/add-new-gig', (req, res) => {
-    res.render('main/add-new-gig');
-})
+router
+    .route('/add-new-gig')
+    .get((req, res) => {
+        res.render('main/add-new-gig');
+    })
+    .post((req, res) => {
+        async.waterfall([function (callback) {
+                let gig = new Gig();
+                gig.owner = req.user._id;
+                gig.title = req.body.gig_title;
+                gig.category = req.body.gig_category;
+                gig.about = req.body.gig_about;
+                gig.price = req.body.gig_price;
+                gig.save(function (err, gig) {
+                    User
+                        .update({
+                            _id: req.user._id
+                        }, {
+                            $push: {
+                                gigs: gig._id
+                            }
+                        }, function (err, count) {
+                            res.redirect('/gigs');
+                        })
+                });
+            }
+        ]);
+    });
 
 module.exports = router;
