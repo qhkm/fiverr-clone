4e3caf33bc60e622cd062a8d5031356923065a64
Qhkm92 <replace-all>
2018-12-18 01:05:51 +0900

    Add sending message feature to the chat

--- realtime/io.js
+++ realtime/io.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const Order = require('../models/gig');
+const async = require('async');
 
 module.exports = function (io) {
 
@@ -9,6 +11,27 @@ module.exports = function (io) {
       console.log(user.name);
       const orderId = socket.request.session.orderId;
       console.log(orderId);
+
+      socket.join(orderId);
+
+      socket.on('chatTo', data => {
+        async.parallel([
+          function (callback) {
+            io
+              . in(orderId)
+              .emit('incomingChat', {
+                message: data.message,
+                sender: user.name,
+                senderImage: user.photo,
+                senderId: user._id
+              });
+          },
+
+          function (callback) {
+            //Save order object
+          }
+        ])
+      })
 
     });
 }
