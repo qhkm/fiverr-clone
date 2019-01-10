b38220499b9d8fc6e2882b1c964f3e535fe82e04
Qhkm92 <replace-all>
2019-01-10 22:06:59 +0900

    Add protected routes

--- routes/order.js
+++ routes/order.js
@@ -5,37 +5,42 @@ const stripe = require('stripe')('sk_test_VBo5DZeMx19wmpLPcJ49esfs');
 const Order = require('../models/order');
 const fee = 3.15;
 const async = require('async');
+const passport = require('passport');
+const passportConfig = require('../config/passport');
 
-router.get('/checkout/single-package/:id', (req, res, next) => {
-    Gig
-        .findOne({
-            _id: req.params.id
-        }, function (err, gig) {
-            var totalPrice = gig.price + fee;
-            req.session.gig = gig;
-            req.session.price = totalPrice;
-            res.render('checkout/single-package', {
-                gig: gig,
-                totalPrice: totalPrice
-            });
-        });
-});
+router.get(
+    '/checkout/single-package/:id',
+    passportConfig.isAuthenticated,
+    (req, res, next) => {
+        Gig.findOne(
+            {
+                _id: req.params.id
+            },
+            function(err, gig) {
+                var totalPrice = gig.price + fee;
+                req.session.gig = gig;
+                req.session.price = totalPrice;
+                res.render('checkout/single-package', {
+                    gig: gig,
+                    totalPrice: totalPrice
+                });
+            }
+        );
+    }
+);
 
 //List all items in the cart
 router.get('/checkout/process_cart', (req, res) => {
-    User
-        .findOne({_id: req.user._id})
+    User.findOne({ _id: req.user._id })
         .populate('cart')
-        .exec(function (err, user) {
+        .exec(function(err, user) {
             console.log(user.cart);
             var price = 0;
             var cartIsEmpty = true;
             if (user.cart.length > 0) {
-                user
-                    .cart
-                    .map(function (item) {
-                        price += item.price;
-                    });
+                user.cart.map(function(item) {
+                    price += item.price;
+                });
                 var totalPrice = price + fee;
             } else {
                 cartIsEmpty = false;
@@ -48,8 +53,8 @@ router.get('/checkout/process_cart', (req, res) => {
                 totalPrice: totalPrice,
                 sub_total: price,
                 cartIsEmpty: cartIsEmpty
-            })
-        })
+            });
+        });
 });
 
 //Single payment
@@ -62,31 +67,34 @@ router
         var gig = req.session.gig;
         var price = req.session.price;
         price *= 100;
-        stripe
-            .customers
-            .create({email: req.user.email})
-            .then((customer) => {
-                return stripe
-                    .customers
-                    .createSource(customer.id, {source: req.body.stripeToken});
+        stripe.customers
+            .create({ email: req.user.email })
+            .then(customer => {
+                return stripe.customers.createSource(customer.id, {
+                    source: req.body.stripeToken
+                });
             })
-            .then((source) => {
-                return stripe
-                    .charges
-                    .create({amount: price, currency: 'usd', customer: source.customer});
+            .then(source => {
+                return stripe.charges.create({
+                    amount: price,
+                    currency: 'usd',
+                    customer: source.customer
+                });
             })
-            .then((charge) => {
+            .then(charge => {
                 var order = new Order();
                 order.buyer = req.user._id;
                 order.seller = gig.owner;
                 order.gig = gig._id;
-                order.save((err) => {
+                order.save(err => {
                     req.session.gig = null;
                     req.session.price = null;
-                    res.redirect('/users/' + req.user._id + '/orders/' + order._id);
-                })
+                    res.redirect(
+                        '/users/' + req.user._id + '/orders/' + order._id
+                    );
+                });
             })
-            .catch((err) => {
+            .catch(err => {
                 // Deal with an error
             });
     });
@@ -101,45 +109,49 @@ router
         var gigs = req.session.gig;
         var price = req.session.price;
         price *= 100;
-        stripe
-            .customers
-            .create({email: req.user.email})
-            .then(function (customer) {
-                return stripe
-                    .customers
-                    .createSource(customer.id, {source: req.body.stripeToken});
-            })
-            .then(function (source) {
-                return stripe
-                    .charges
-                    .create({amount: price, currency: 'usd', customer: source.customer});
-            })
-            .then(function (charge) {
-                // DO SOMETHING
-                gigs
-                    .map(function (gig) {
-                        var order = new Order();
-                        order.buyer = req.user._id;
-                        order.seller = gig.owner;
-                        order.gig = gig._id;
-                        order.save(function (err) {
-                            req.session.gig = null;
-                            req.session.price = null;
-                        });
-                    });
-                User.update({
-                    _id: req.user._id
-                }, {
-                    $set: {
-                        cart: []
-                    }
-                }, function (err, updated) {
-                    if (updated) {
-                        res.redirect('/users/' + req.user._id + '/orders');
-                    }
+        stripe.customers
+            .create({ email: req.user.email })
+            .then(function(customer) {
+                return stripe.customers.createSource(customer.id, {
+                    source: req.body.stripeToken
                 });
             })
-            .catch(function (err) {
+            .then(function(source) {
+                return stripe.charges.create({
+                    amount: price,
+                    currency: 'usd',
+                    customer: source.customer
+                });
+            })
+            .then(function(charge) {
+                // DO SOMETHING
+                gigs.map(function(gig) {
+                    var order = new Order();
+                    order.buyer = req.user._id;
+                    order.seller = gig.owner;
+                    order.gig = gig._id;
+                    order.save(function(err) {
+                        req.session.gig = null;
+                        req.session.price = null;
+                    });
+                });
+                User.update(
+                    {
+                        _id: req.user._id
+                    },
+                    {
+                        $set: {
+                            cart: []
+                        }
+                    },
+                    function(err, updated) {
+                        if (updated) {
+                            res.redirect('/users/' + req.user._id + '/orders');
+                        }
+                    }
+                );
+            })
+            .catch(function(err) {
                 // Deal with an error
             });
     });
@@ -147,19 +159,18 @@ router
 //Chat page
 router.get('/users/:userId/orders/:orderId', (req, res, next) => {
     req.session.orderId = req.params.orderId;
-    Order
-        .findOne({_id: req.params.orderId})
+    Order.findOne({ _id: req.params.orderId })
         .populate('buyer')
         .populate('seller')
         .populate('gig')
         .deepPopulate('messages.owner')
-        .exec(function (err, order) {
+        .exec(function(err, order) {
             console.log(order);
             res.render('order/order-room', {
                 layout: 'chat-layout',
                 order: order,
                 helpers: {
-                    if_equals: function (a, b, opts) {
+                    if_equals: function(a, b, opts) {
                         if (a.equals(b)) {
                             return opts.fn(this);
                         } else {
@@ -172,67 +183,73 @@ router.get('/users/:userId/orders/:orderId', (req, res, next) => {
 });
 
 router.get('/users/:id/manage_orders', (req, res, next) => {
-    Order
-        .find({seller: req.user._id})
+    Order.find({ seller: req.user._id })
         .populate('buyer')
         .populate('seller')
         .populate('gig')
         .exec((err, orders) => {
-            res.render('order/order-seller', {orders: orders});
+            res.render('order/order-seller', { orders: orders });
         });
 });
 
 router.get('/users/:id/orders', (req, res, next) => {
-    Order
-        .find({buyer: req.user._id})
+    Order.find({ buyer: req.user._id })
         .populate('buyer')
         .populate('seller')
         .populate('gig')
         .exec((err, orders) => {
-            res.render('order/order-buyer', {orders: orders});
+            res.render('order/order-buyer', { orders: orders });
         });
 });
 
 //Add item to cart
 router.post('/add-to-cart', (req, res, next) => {
     const gigId = req.body.gig_id;
-    User.update({
-        _id: req.user._id
-    }, {
-        $push: {
-            cart: gigId
+    User.update(
+        {
+            _id: req.user._id
+        },
+        {
+            $push: {
+                cart: gigId
+            }
+        },
+        function(err, count) {
+            res.json('Added to cart');
         }
-    }, function (err, count) {
-        res.json("Added to cart");
-    });
+    );
 });
 
 //Remove item from cart
 router.post('/remove-item', (req, res, next) => {
     const gigId = req.body.gig_id;
     async.waterfall([
-        function (callback) {
-            Gig
-                .findOne({
+        function(callback) {
+            Gig.findOne(
+                {
                     _id: gigId
-                }, function (err, gig) {
+                },
+                function(err, gig) {
                     callback(err, gig);
-                })
-
+                }
+            );
         },
 
-        function (gig, callback) {
-            User
-                .update({
+        function(gig, callback) {
+            User.update(
+                {
                     _id: req.user._id
-                }, {
+                },
+                {
                     $pull: {
                         cart: gigId
                     }
-                }, function (err, count) {
+                },
+                function(err, count) {
                     var totalPrice = req.session.price - gig.price;
-                    res.json({totalPrice: totalPrice, price: gig.price});
-                });
+                    res.json({ totalPrice: totalPrice, price: gig.price });
+                }
+            );
         }
     ]);
 });
